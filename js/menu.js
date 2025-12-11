22c392a88aee0cd6000ab043df252520b6060e2c
sveta <svetagimish@gmail.com>
2025-12-11 22:06:01 +0200

    close mob-menu

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,20 @@
 (() => {
-    const refs = {
-        // Додати атрибут data-modal-open на кнопку відкриття
-        openModalBtn: document.querySelector("[data-menu-open]"),
-        // Додати атрибут data-modal-close на кнопку закриття
-        closeModalBtn: document.querySelector("[data-menu-close]"),
-        // Додати атрибут data-modal на бекдроп модалки
-        modal: document.querySelector("[data-menu]"),
+    const refs = {  
+        openModalBtn: document.querySelector("[data-menu-open]"),// Додати атрибут data-modal-open на кнопку відкриття
+        closeModalBtn: document.querySelector("[data-menu-close]"),// Додати атрибут data-modal-close на кнопку закриття
+        modal: document.querySelector("[data-menu]"), // Додати атрибут data-modal на бекдроп модалки
+        menuLinks: document.querySelectorAll("[data-menu] a"), // всі лінки в меню
     };
 
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  // Закриваємо меню при натисканні на кожну лінку
+  refs.menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      refs.modal.classList.remove("is-open");
+    });
+  });
 
     function toggleModal() {
         // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
